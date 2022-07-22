3734c16d9dd401f5dba5e2385a37eac156a57c2f
Dylan Bruner <brunerdylan5@gmail.com>
2022-07-22 16:02:54 -0400

    Fix Gps & Add a program to test all devices

--- vexdevices/virtualvision.py
+++ vexdevices/virtualvision.py
@@ -9,18 +9,19 @@ class LargestObject(object):
         self.height  = height
         self.angle   = angle
         self.exists  = exists
+
 class Vision(object):
     def __init__(self):
         self._attributes = {
-            {'type': 'float',  'name': 'width',   'value': 0.0},
-            {'type': 'float',  'name': 'height',  'value': 0.0},
-            {'type': 'flaot',  'name': 'angle',   'value': 0.0},
-            {'type': 'bool',   'name': 'exists',  'value': False},
-            {'type': 'float',  'name': 'originX', 'value': 0.0},
-            {'type': 'float',  'name': 'originY', 'value': 0.0},
-            {'type': 'float',  'name': 'centerX', 'value': 0.0},
-            {'type': 'float',  'name': 'centerY', 'value': 0.0},
-            {'type': 'string', 'name': 'id',      'value': ''}
+            'id': {'type': 'string', 'value': ''},
+            'originX': {'type': 'float', 'value': 0.0},
+            'originY': {'type': 'float', 'value': 0.0},
+            'centerX': {'type': 'float', 'value': 0.0},
+            'centerY': {'type': 'float', 'value': 0.0},
+            'width': {'type': 'float', 'value': 0.0},
+            'height': {'type': 'float', 'value': 0.0},
+            'angle': {'type': 'float', 'value': 0.0},
+            'exists': {'type': 'bool', 'value': False}
         }
     
     def get_largest_object(self) -> LargestObject:
